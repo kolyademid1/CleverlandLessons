71e491ed29e15b2b779cade59f7415ffbeb27049
kolyademid1 <73250003+kolyademid1@users.noreply.github.com>
2021-03-15 21:17:01 +0300

    Fixed error-message

--- Lesson04/4.2/script05.js
+++ Lesson04/4.2/script05.js
@@ -7,27 +7,26 @@ function start05(){
     
     let N = +prompt ('Введите единицу измерения (1-дм/ 2-км/ 3-м/ 4-мм/ 5-см)');
     let L = +prompt ('Введите количество');
-    let result =0;
+    let result ='';
     switch (N) {
         case 1:
             result=`${L*DECIMETRE} метров`;
-            
             break;
         case 2:
-            result = `${L*KILOMETRE} метров`;
+            result = `Получается ${L*KILOMETRE} метров`;
             break;
         case 3:
-            result = `${L*METRE} метров`;
+            result = `Получается ${L*METRE} метров`;
             break;
         case 4:
-            result = `${L*MILLIMETRE} метров`;
+            result = `Получается ${L*MILLIMETRE} метров`;
             break;
         case 5:
-            result = `${L*CENTIMETRE} метров`;
+            result = `Получается ${L*CENTIMETRE} метров`;
             break;
-        default: alert ('Неизвестная единица измерения');
+        default: 
+            result='Неизвестная единица измерения';
             break;
-    
     }
-        alert (` Получается ${result}`)
+    alert (result);
 }
