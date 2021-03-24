aad8f9779c7ee695d516aad4ff2b1a35782aa749
kolyademid1 <73250003+kolyademid1@users.noreply.github.com>
2021-03-24 20:16:57 +0300

    Added ex1 lesson06

---
+++ JS/Lesson06/script01.2.js
@@ -0,0 +1,12 @@
+function start09(){
+    let n = +prompt ('Введите n');
+    let m = +prompt ('Введите m')
+    let result = multiply (n,m);
+    alert (result);
+}
+
+ function multiply (n,m) {
+     return n * m;
+ }
+
+ 
